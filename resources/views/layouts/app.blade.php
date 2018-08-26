b9fbba0434e833b48fac54726bff8efede5b3a51
Mahmoud Gomaa <mahmoudg77@gmail.com>
2018-08-27 00:18:42 +0200

    Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -165,41 +165,49 @@
 <footer class="footer_area">
         <div class="container">
             <div class="footer_row row">
-                <div class="col-md-3 col-sm-6 footer_about">
+                <div class="col-md-4 col-sm-6 footer_about">
                     <h2>ABOUT OUR COMPANY</h2>
                     <img src="/images/logo.png" alt="">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                    <p>{{Setting::getIfExists('site_desc','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')}}</p>
+
                     <ul class="socail_icon">
-                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        @foreach(Func::menu('social_links_footer') as $link)
+                            {!!  Func::drowMenuLink($link)!!}
+                        @endforeach
+                        {{--<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>--}}
+                        {{--<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>--}}
+                        {{--<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>--}}
+                        {{--<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>--}}
                     </ul>
                 </div>
-                <div class="col-md-3 col-sm-6 footer_about quick">
+                <div class="col-md-4 col-sm-6 footer_about quick">
                     <h2>Quick links</h2>
                     <ul class="quick_link">
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Building Construction</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Home Renovation</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Hardwood Flooring</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Repairing Of Roof</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Commercial Construction</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Concreate Transport</a></li>
+                        @foreach(Func::menu('quick_links_footer') as $link)
+                            {!!  Func::drowMenuLink($link)!!}
+                        @endforeach
+                        {{--<li><a href="#"><i class="fa fa-chevron-right"></i>Building Construction</a></li>--}}
+                        {{--<li><a href="#"><i class="fa fa-chevron-right"></i>Home Renovation</a></li>--}}
+                        {{--<li><a href="#"><i class="fa fa-chevron-right"></i>Hardwood Flooring</a></li>--}}
+                        {{--<li><a href="#"><i class="fa fa-chevron-right"></i>Repairing Of Roof</a></li>--}}
+                        {{--<li><a href="#"><i class="fa fa-chevron-right"></i>Commercial Construction</a></li>--}}
+                        {{--<li><a href="#"><i class="fa fa-chevron-right"></i>Concreate Transport</a></li>--}}
                     </ul>
                 </div>
-                <div class="col-md-3 col-sm-6 footer_about">
-                    <h2>Twitter Feed</h2>
-                    <a href="#" class="twitter">@colorlib: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</a>
-                    <a href="#" class="twitter">@colorlib: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</a>
-                </div>
-                <div class="col-md-3 col-sm-6 footer_about">
+                {{--<div class="col-md-3 col-sm-6 footer_about">--}}
+                    {{--<h2>Twitter Feed</h2>--}}
+                    {{--<a href="#" class="twitter">@colorlib: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</a>--}}
+                    {{--<a href="#" class="twitter">@colorlib: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</a>--}}
+                {{--</div>--}}
+                <div class="col-md-4 col-sm-6 footer_about">
                     <h2>CONTACT US</h2>
                     <address>
                         <p>Have questions, comments or just want to say hello:</p>
                         <ul class="my_address">
-                            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>info@omegaegy.net</a></li>
-                            <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i>+202 2795 1402</a></li>
-                            <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span>17 Magles El Shaab St. Lazogly Square Cairo, Egypt </span></a></li>
+                            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>{{Setting::getIfExists('site_email','info@omegaegy.net')}}</a></li>
+                            <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i>{{Setting::getIfExists('site_phone','+202 2795 1402')}}</a></li>
+                            <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span>{{Setting::getIfExists('site_address','17 Magles El Shaab St. Lazogly Square Cairo, Egypt')}} </span></a></li>
                         </ul>
                     </address>
                 </div>
